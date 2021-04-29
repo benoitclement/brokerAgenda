d1f97191d1ec8a8e584e1aea0761f9a718de65ec
benoit.clement <benoit.cle@gmail.com>
2021-04-29 22:52:18 +0200

    simplified copy of appointment index controller/view into HomePage

--- brokerAgenda/Controllers/HomeController.cs
+++ brokerAgenda/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using brokerAgenda.Models;
+﻿using brokerAgenda.Data;
+using brokerAgenda.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,14 +14,42 @@ namespace brokerAgenda.Controllers
   {
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, BrokerAppDbContext db)
     {
       _logger = logger;
+      _db = db;
     }
-
-    public IActionResult Index()
+    private readonly BrokerAppDbContext _db;
+    //public HomeController(BrokerAppDbContext db)
+    //{
+    //  _db = db;
+    //}
+    public IActionResult Index(string sortOrder, string searchString)
     {
-      return View();
+      IEnumerable<Appointment> appointmentList = _db.Appointments;
+      foreach (var appointment in appointmentList)
+      {
+        appointment.IdBrokerNavigation = _db.Brokers.FirstOrDefault(b => b.Id == appointment.IdBroker);
+        appointment.IdCustomerNavigation = _db.Customers.FirstOrDefault(c => c.Id == appointment.IdCustomer);
+      }
+      if (!String.IsNullOrEmpty(searchString))
+      {
+        searchString = searchString.ToLower();
+        appointmentList = appointmentList.Where(a =>
+          a.IdBrokerNavigation.Lastname.ToLower().Contains(searchString)
+          || a.IdBrokerNavigation.Firstname.ToLower().Contains(searchString)
+          || a.IdCustomerNavigation.Lastname.ToLower().Contains(searchString)
+          || a.IdCustomerNavigation.Firstname.ToLower().Contains(searchString)
+        );
+      }
+      appointmentList = sortOrder switch //new syntax for switch _ is default
+      {
+        "BrokerName" => appointmentList.OrderBy(row => row.IdBrokerNavigation.Lastname),
+        "CustomerName" => appointmentList.OrderBy(row => row.IdCustomerNavigation.Lastname),
+        "DateTime" => appointmentList.OrderBy(row => row.DateHour),
+        _ => appointmentList.OrderBy(row => row.IdBrokerNavigation.Lastname),
+      };
+      return View(appointmentList);
     }
 
     public IActionResult Help()
