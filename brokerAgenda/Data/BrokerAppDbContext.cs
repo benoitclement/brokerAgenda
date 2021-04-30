854e95df276db64c783c34d2ed26c09d2a567e46
benoit.clement <benoit.cle@gmail.com>
2021-04-30 10:57:26 +0200

    wip: deploy to azure

--- brokerAgenda/Data/BrokerAppDbContext.cs
+++ brokerAgenda/Data/BrokerAppDbContext.cs
@@ -26,7 +26,7 @@ namespace brokerAgenda.Data
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer("Name=localhost");
+                optionsBuilder.UseSqlServer("Name=BrokerAgendaDbConnection");
             }
         }
 
